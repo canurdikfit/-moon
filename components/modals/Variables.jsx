e76d3ea9209cc37140fe4953f33ae8845a8f6a30
canurdikfit <161978264+canurdikfit@users.noreply.github.com>
2024-04-13 10:43:14 +0100

    Update Variables.jsx

--- components/modals/Variables.jsx
+++ components/modals/Variables.jsx
@@ -6,14 +6,14 @@ import {
 } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import React, { useRef } from "react";
-import { motion } from "framer-motion-3d";
+// import { motion } from "framer-motion-3d";
 
 function Variables() {
-  const moonOrbit = useRef(null);
+  // const moonOrbit = useRef(null);
   const bgStar = useRef(null);
-  const colorMapTexture = useTexture("/8k_moon.jpg");
+  // const colorMapTexture = useTexture("/8k_moon.jpg");
   useFrame((state, delta) => {
-    moonOrbit.current.rotation.y += delta * 0.05;
+    // moonOrbit.current.rotation.y += delta * 0.05;
     bgStar.current.rotation.y += delta * 0.0009;
   });
 
@@ -28,7 +28,7 @@ function Variables() {
       <ambientLight intensity={0.01} />
       <directionalLight args={["#ffad14", 0.4]} position={[2, 1, 0]} />
       <directionalLight args={["#fff", 0.8]} position={[2, 1, 0]} />
-      <motion.mesh
+      {/* <motion.mesh
         position={[0, 0, 0]}
         rotation={[0, angleToRadians(90), 0]}
         ref={moonOrbit}
@@ -45,7 +45,7 @@ function Variables() {
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial color="blue" />
         <meshStandardMaterial map={colorMapTexture} />
-      </motion.mesh>
+      </motion.mesh> */}
       <mesh position={[0, 0, -10]} ref={bgStar}>
         <Stars
           radius={70}
